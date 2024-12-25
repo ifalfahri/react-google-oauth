52306bb456facf8321eaa429719872e4f4c55ded
IfalEXZ <ifaltampan69@gmail.com>
2024-12-25 14:21:47 +0800

    feat: implement Google login functionality and remove unused CSS file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import useState
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+//import useGoogleLogin
+import { useGoogleLogin } from '@react-oauth/google';
+
+const App = () => {
+
+  // state access token
+  const [accessToken, setAccessToken] = useState(localStorage.getItem('accessToken'));
+
+  // function to handle login success
+  const handleLoginSuccess = (response) => {
+
+    // get access token from response
+    const token = response.access_token;
+
+    //set token in state
+    setAccessToken(token);
+
+    // set token in localStorage
+    localStorage.setItem('accessToken', token);
+  };
+
+  // function to handle login error
+  const handleLoginError = (error) => {
+    console.error('Login Failed:', error);
+  };
+
+  // function to login with google
+  const login = useGoogleLogin({
+    onSuccess: handleLoginSuccess,
+    onError: handleLoginError,
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container mt-5'>
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card border-0 rounded shadow-sm">
+            <div className="card-header">
+              <h5 className="card-title mb-0">Google Login with React</h5>
+            </div>
+            <div className="card-body">
+              <div className='text-center'>
+                {
+                  accessToken
+                    ? <div className='alert alert-success'> Login Success.</div>
+                    : ''
+                }
+                <button className="btn btn-primary" onClick={() => login()}>Login with Google</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
