ede18a5b693c4f3834a8fa8e954f0057d70292a4
IfalEXZ <ifaltampan69@gmail.com>
2024-12-25 14:20:41 +0800

    add: integrate Google OAuth provider and remove unused CSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId="32299594467-vbjjquvr1noal1ses31m1iqs7m4vl4s2.apps.googleusercontent.com">
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </GoogleOAuthProvider>,
 )
